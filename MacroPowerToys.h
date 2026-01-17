b2083e6d7f443827679353c6897d7c9d4ac423ea
Neko Box Coder <nekoboxcoder@gmail.com>
2026-01-17 01:47:15 +0000

    Using internal GET_COUNT macro instead

--- MacroPowerToys.h
+++ MacroPowerToys.h
@@ -3,10 +3,10 @@
 #include "./Miscellaneous.h"
 #include "./RemoveParenthesisInList.h"
 #include "./AppendListsItems.h"
-#include "./ArgsCount.h"                    //INTERNAL_MPT_GET_COUNT, INTERNAL_MPT_GET_COUNT2
+#include "./ArgsCount.h"
 #include "./ConcatListsItems.h"
 #include "./GetLastArg.h"
-#include "./AreArgsEmpty.h"                 //INTERNAL_MPT_GET_COUNT3, INTERNAL_MPT_GET_COUNT4
+#include "./AreArgsEmpty.h"
 #include "./Overload.h"
 #include "./PrefixSuffixArgs.h"             //INTERNAL_MPT_ARE_ARGS_EMPTY
 #include "./PrependAppendArgs.h"            //INTERNAL_MPT_ARE_ARGS_EMPTY
